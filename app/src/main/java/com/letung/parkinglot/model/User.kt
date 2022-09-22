e662f801d29363bd9948cf59dd46e3acdb3013ef
LeCongTung <tung.100301@gmail.com>
2022-09-22 17:04:56 +0700

    update

---
+++ app/src/main/java/com/letung/parkinglot/model/User.kt
@@ -0,0 +1,8 @@
+package com.letung.parkinglot.model
+
+data class User(
+    var name: String = "",
+    var familyName: String = "",
+    var phoneNumber: String = "",
+    var identityCard: String = ""
+)
