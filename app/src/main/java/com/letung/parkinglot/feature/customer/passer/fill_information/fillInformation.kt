89aef11ff720f554cc214d0cbccff13d64245c03
dangkhoa1404 <khoadang.1404@gmail.com>
2022-09-24 23:28:51 +0700

    update 11h28: Form identity passer

--- app/src/main/java/com/letung/parkinglot/feature/customer/passer/fill_information/fillInformation.kt
+++ app/src/main/java/com/letung/parkinglot/feature/customer/passer/fill_information/fillInformation.kt
@@ -2,11 +2,30 @@ package com.letung.parkinglot.feature.customer.passer.fill_information
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.letung.parkinglot.R
 
 class fillInformation : AppCompatActivity() {
+    val carType = arrayOf("Xe 5 chỗ", "Xe 7 chỗ", "Xe 9 chỗ", "Xe 16 chỗ")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fill_information)
+
+        val spinner = findViewById<Spinner>(R.id.spinner_carType)
+        val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, carType)
+        spinner.adapter = arrayAdapter
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+        }
     }
 }
