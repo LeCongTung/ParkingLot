6f218db1cf163bfd34bc2dca8326cba101256d79
LeCongTung <tung.100301@gmail.com>
2022-10-02 16:36:32 +0700

    update 2/10 16 models

--- app/src/main/java/com/letung/parkinglot/model/Slot.kt
+++ app/src/main/java/com/letung/parkinglot/model/Slot.kt
@@ -2,5 +2,12 @@ package com.letung.parkinglot.model
 
 data class Slot(
     val position: String ?= null,
-    val status: String ? = null
+    val status: String ? = null,
+    val infoSlot: ArrayList<ParkingSlot> ?= null
+)
+
+data class ParkingSlot(
+    val time: String ?= null,
+    val check: Boolean ?= null,
+    val idTicket: String ?= null
 )
