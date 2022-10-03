7052e97666119193c5b0a650155e46daf9731dd3
dangkhoa1404 <khoadang.1404@gmail.com>
2022-10-03 10:09:55 +0700

    update 3/10/2022: form payment

---
+++ app/src/main/java/com/letung/parkinglot/feature/bill/invoiceActivity.kt
@@ -0,0 +1,12 @@
+package com.letung.parkinglot.feature.bill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.letung.parkinglot.R
+
+class invoiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_invoice)
+    }
+}
