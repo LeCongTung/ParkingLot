0d071a7a621ef5db2d6a90de91c03e9e0522bf7f
dangkhoa1404 <khoadang.1404@gmail.com>
2022-10-28 09:53:45 +0700

    profile form

---
+++ app/src/main/java/com/letung/parkinglot/feature/profile/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.letung.parkinglot.feature.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.letung.parkinglot.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
