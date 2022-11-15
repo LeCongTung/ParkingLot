aca48ef5c574e4d9e3f3c65432dfe341d3ffe282
dangkhoa1404 <khoadang.1404@gmail.com>
2022-11-15 23:16:42 +0700

    update 23h17: search in bottom sheet dialog

--- app/src/main/java/com/letung/parkinglot/feature/depositUserAccount/depositUserAccountActivity.kt
+++ app/src/main/java/com/letung/parkinglot/feature/depositUserAccount/depositUserAccountActivity.kt
@@ -2,10 +2,10 @@ package com.letung.parkinglot.feature.depositUserAccount
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import androidx.appcompat.widget.SearchView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.doAfterTextChanged
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialog
@@ -14,13 +14,9 @@ import com.letung.parkinglot.R
 import com.letung.parkinglot.feature.depositUserAccount.adapter.BankAdapter
 import com.letung.parkinglot.model.UserBank
 import kotlinx.android.synthetic.main.activity_deposit_user_account.*
-import kotlinx.android.synthetic.main.user_banklist.*
-import kotlinx.android.synthetic.main.user_bottom_sheet.*
-import java.text.DecimalFormat
-import java.text.NumberFormat
-import java.util.*
 import kotlin.collections.ArrayList
-import androidx.core.view.contains
+import kotlinx.android.synthetic.main.user_bottom_sheet.view.*
+import java.util.*
 
 class depositUserAccountActivity : AppCompatActivity() {
     private lateinit var database: DatabaseReference
@@ -32,10 +28,11 @@ class depositUserAccountActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_deposit_user_account)
         database = FirebaseDatabase.getInstance().getReference("Bank")
+
         userArraylist = arrayListOf<UserBank>()
         tempArrayList = arrayListOf<UserBank>()
         eventListener()
-        //searchData()
+        backToUserProfile()
     }
 
     private fun getUserData() {
@@ -48,7 +45,7 @@ class depositUserAccountActivity : AppCompatActivity() {
                         tempArrayList.add(user!!)
                         Log.d("tung123123", userSnapshot.toString())
                     }
-                    var adapter = BankAdapter(this@depositUserAccountActivity, userArraylist)
+                    var adapter = BankAdapter(this@depositUserAccountActivity, tempArrayList)
                     userRecyclerview.adapter = adapter
                     adapter.setOnItemClickListener(object : BankAdapter.onItemClickListener{
                         override fun onItemClick(position: Int) {
@@ -66,7 +63,7 @@ class depositUserAccountActivity : AppCompatActivity() {
     private fun showBottomSheet() {
         val dialog = BottomSheetDialog(this, R.style.BottomSheetDialogTheme)
         val view = layoutInflater.inflate(R.layout.user_bottom_sheet, null)
-        dialog.setCancelable(true)
+        dialog.setCancelable(false)
         dialog.setContentView(view)
         userRecyclerview = dialog.findViewById(R.id.bottom_bankList)!!
         userRecyclerview.layoutManager = LinearLayoutManager(this)
@@ -77,12 +74,51 @@ class depositUserAccountActivity : AppCompatActivity() {
         dialog.behavior.maxHeight = 1500
         dialog.behavior.peekHeight = dialog.behavior.maxHeight
         dialog.show()
+        val searchBank = view.searchView
+        val img_close = view.img_closeDialog
+        //view.searchView
+        searchData(searchBank)
+        closeDialog(dialog, img_close)
     }
 
-    private fun searchData(){
+    private fun closeDialog(dialog: BottomSheetDialog, imgClose: ImageView) {
+        imgClose.setOnClickListener(){
+            tempArrayList.clear()
+            dialog.dismiss()
+        }
+    }
+
+
+    private fun searchData(searchBank: SearchView) {
+        searchBank.clearFocus()
+        searchBank.setOnQueryTextListener(object :SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filterList(newText)
+                return true
+            }
+
+        })
     }
 
     private fun filterList(newText: String?) {
+        tempArrayList.clear()
+        val searchText = newText!!.toLowerCase(Locale.getDefault())
+        if(searchText.isNotEmpty()){
+            userArraylist.forEach{
+                if(it.name!!.toLowerCase(Locale.getDefault()).contains(searchText)){
+                    tempArrayList.add(it)
+                }
+            }
+            userRecyclerview.adapter!!.notifyDataSetChanged()
+        }else{
+            tempArrayList.clear()
+            tempArrayList.addAll(userArraylist)
+            userRecyclerview.adapter!!.notifyDataSetChanged()
+        }
     }
 
 
@@ -92,4 +128,14 @@ class depositUserAccountActivity : AppCompatActivity() {
         }
     }
 
+    private fun backToUserProfile() {
+        imgbtn_backToMainActivity.setOnClickListener(){
+            onBackPressed()
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+    }
 }
