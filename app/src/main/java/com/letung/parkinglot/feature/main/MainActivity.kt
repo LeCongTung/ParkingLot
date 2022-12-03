df527e870ebfdea2d66c5266d9e3a1a51aff0b5e
dangkhoa1404 <khoadang.1404@gmail.com>
2022-12-03 15:38:56 +0700

    update 15h39: update mainActivity UI p1

--- app/src/main/java/com/letung/parkinglot/feature/main/MainActivity.kt
+++ app/src/main/java/com/letung/parkinglot/feature/main/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.letung.parkinglot.feature.main
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -10,6 +12,7 @@ import com.letung.parkinglot.feature.chooseUserCar.ChooseUserCarActivity
 import com.letung.parkinglot.feature.parking.ParkingActivity
 import com.letung.parkinglot.feature.profile.ProfileActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_newfeature_notice.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,16 +22,31 @@ class MainActivity : AppCompatActivity() {
         eventListener()
     }
     private fun eventListener(){
-        btn_moveToProfile.setOnClickListener(){
-//            Toast.makeText(this, "chuyển sang ${Account.DATA_NAME}", Toast.LENGTH_SHORT).show()
+        img_userProfile.setOnClickListener(){
             startActivity(Intent(this, ProfileActivity::class.java))
         }
-
-        btn_moveToParking.setOnClickListener(){
-//            Toast.makeText(this, "chuyển sang ${Account.DATA_NAME}", Toast.LENGTH_SHORT).show()
+        car_parking.setOnClickListener(){
             startActivity(Intent(this, ChooseUserCarActivity::class.java))
+        }
+        car_rent.setOnClickListener(){
+            setDialogNewFeatureNotice()
+        }
+        car_washing.setOnClickListener(){
+            setDialogNewFeatureNotice()
         }
     }
 
+    private fun setDialogNewFeatureNotice(){
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_newfeature_notice)
+        if(dialog.window != null){
+            dialog!!.window!!.setBackgroundDrawable(ColorDrawable(0))
+        }
+        dialog.setCancelable(false)
+        dialog.show()
+        dialog.btn_continueDismiss.setOnClickListener(){
+            dialog.dismiss()
+        }
+    }
 
 }
